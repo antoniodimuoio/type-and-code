085f7e329ee5faa9ab83bce03aa0c3f58fbc8100
antoniodimuoio <antoniodimuoio2004@gmail.com>
2025-03-21 10:13:02 +0100

    Update code.js

--- code.js
+++ code.js
@@ -6,7 +6,7 @@ export const configurazione = {
   allineamento: "centro",
   percorsoFont: "./assets/InputMonoCondensed-BoldItalic.ttf",
 
-  sensibilitàMicrofonoBase: 30,
+  sensibilitàMicrofonoBase: 60,
   densitàPuntiBase: 0.5,
 
   nascondiInterfaccia: true,
